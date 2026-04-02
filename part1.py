131b2d0e189fa3e867f53eb95661e14ebcebc4af
Tom Bini <tom.bini04@gmail.com>
2026-04-02 21:27:17 +0200

    Started simul

---
+++ part1.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy import integrate
+
+t_end = 3 #secondes, la durée de simulation
+
+
+
+def cas_1():
+    
+    y0 = np.array([0, 0, 0, 0, 0, 0])
+    
+    pass
+
+def cas_2():
+    
+    y0 = np.array([0, 0, 0, 0, 0, 0])
+    
+    pass
+
+def cas_3():
+    
+    y0 = np.array([0, 0, 0, 0, 0, 0])
+    
+    pass
+
+integrate.solve_ivp()

33a912124e7dfa9f625cf7c32a3025bec603b82e
Tom Bini <tom.bini04@gmail.com>
2026-04-06 23:08:20 +0200

    Question 2.4 compute des eigenvalues

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import integrate
 import visualization as vz
 import matplotlib.pyplot as plt
+import sympy as sp
 
 #----------------------------------------QUESTION 1.6----------------------------------------
 
@@ -73,13 +74,11 @@ def linear_system(t, state_variables, u_s, u_d):
     matrix_A[1,4] = 1
     matrix_A[2,5] = 1
 
-    matrix_A[3,2] = - u_s / m * np.cos(phi)
-    matrix_A[4,2] = - u_s / m * np.sin(phi)
+    matrix_A[3,2] = - u_s / m
     
     matrix_B = np.zeros((6, 2))
     
-    matrix_B[3,0] = - np.sin(phi) / m
-    matrix_B[4,0] = np.cos(phi) / m
+    matrix_B[4,0] = 1 / m
     matrix_B[5,1] = l / (2 * inertia)
     
     matrix_C = np.zeros((2, 6))
@@ -171,60 +170,64 @@ def case_noisy():
     
 #----------------------------------------QUESTION 2.2----------------------------------------
 #Position:
-x = None
-y = None
-phi = None
-
-#Position de référence:
-x_ref = None
-y_ref = None
-phi_ref = None
-
-#Gains (> 0):
-k_x = None
-k_y = None
-k_phi = None
+x, y, phi, x_ref, y_ref, phi_ref, k_x, k_y, k_phi = sp.symbols("x y phi x_ref y_ref phi_ref k_x k_y k_phi")
+l, m, inertia, g = sp.symbols("l m inertia g")
 
 u_s = m * (g + k_y * (y_ref - y))
-phi_ref = - 1/g * k_x * (x_ref - x)
-u_d = 2 * inertia / l * k_phi * (phi_ref - phi)
+u_d = 2 * inertia / l * k_phi * (- 1/g * k_x * (x_ref - x) - phi)
 
 #----------------------------------------QUESTION 2.3----------------------------------------
 
-matrix_A = np.zeros((6, 6))
+matrix_A = sp.zeros(6, 6)
 
 matrix_A[0,3] = 1
 matrix_A[1,4] = 1
 matrix_A[2,5] = 1
 
-matrix_A[3,2] = - u_s / m * np.cos(phi)
-matrix_A[4,2] = - u_s / m * np.sin(phi)
+matrix_A[3,2] = - g
 
-matrix_B = np.zeros((6, 2))
+matrix_B = sp.zeros(6, 2)
 
-matrix_B[3,0] = - np.sin(phi) / m
-matrix_B[4,0] = np.cos(phi) / m
+matrix_B[4,0] = 1 / m
 matrix_B[5,1] = l / (2 * inertia)
 
-matrix_C = np.zeros((2, 6))
+matrix_C = sp.zeros(2, 6)
 
 matrix_C[0,0] = 1
 matrix_C[1,1] = 1
 
-matrix_D = np.zeros((2,2))
+matrix_D = sp.zeros(2,2)
 
-matrix_K = np.zeros((2, 6))
+matrix_K = sp.zeros(2,6)
 
-matrix_K[0][1] = m * k_y
-matrix_K[1][0] = -2 * inertia * k_phi * k_x / (g * l)
-matrix_K[1][2] = 2 * inertia * k_phi / l
+matrix_K[0,1] = m * k_y
+matrix_K[1,0] = -2 * inertia * k_phi * k_x / (g * l)
+matrix_K[1,2] = 2 * inertia * k_phi / l
+sp.pprint(matrix_B)
 
-matrix_Kr = np.zeros((2, 2))
+matrix_Kr = sp.zeros(2, 2)
 
-matrix_Kr[0][1] = m * k_y
-matrix_Kr[1][0] = -2 * inertia * k_phi * k_x / (g * l)
+matrix_Kr[0,1] = m * k_y
+matrix_Kr[1,0] = -2 * inertia * k_phi * k_x / (g * l)
 
-matrix_A_tilde = matrix_A - matrix_B @ matrix_K
-matrix_B_tilde = matrix_B @ matrix_Kr
-matrix_C_tilde = matrix_C - matrix_D @ matrix_K
-matrix_D_tilde = matrix_D @ matrix_Kr
+matrix_A_tilde = matrix_A - (matrix_B * matrix_K)
+
+matrix_B_tilde = matrix_B * matrix_Kr
+matrix_C_tilde = matrix_C - matrix_D * matrix_K
+matrix_D_tilde = matrix_D * matrix_Kr
+
+#----------------------------------------QUESTION 2.4----------------------------------------
+
+matrix_A_tilde_sp = sp.Matrix(matrix_A_tilde)
+
+eigenvalues_matrix_A_tilde = matrix_A_tilde_sp.eigenvals()
+print("Matrix A :")
+sp.pprint(matrix_A)
+print("Matrix B :")
+sp.pprint(matrix_B)
+print("Matrix K :")
+sp.pprint(matrix_K)
+print("Matrix A tilde:")
+sp.pprint(matrix_A_tilde)
+sp.pprint(eigenvalues_matrix_A_tilde)
+sp.preview(eigenvalues_matrix_A_tilde, viewer='file', filename='output.png')
